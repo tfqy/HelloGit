58f8590f95c94a0de1e0ffac5b3a5c5916a24320
tfqy <1115142237@qq.com>
2021-11-22 13:09:34 +0800

    主干添加

--- src/main/java/com/tfqy/git/helloGit.java
+++ src/main/java/com/tfqy/git/helloGit.java
@@ -6,5 +6,6 @@ public class helloGit {
         System.out.println("hello Git");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 }
